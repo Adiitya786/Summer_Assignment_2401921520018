fea3526a144c232408d82dd47437e8439e0c3f5c
Adiitya786 <adityaprajapatii786@gmail.com>
2026-06-17 00:32:58 +0530

    Valid Paranthesis

---
+++ Week_3/Day_3/ValidPara.java
@@ -0,0 +1,33 @@
+package Week_3.Day_3;
+class Solution {
+    public boolean isValid(String str) {
+          Stack<Character> s = new Stack<>();
+
+        for(int i =0;i<str.length();i++){
+             char ch = str.charAt(i);
+             // opening
+             if(ch == '{' || ch =='(' || ch =='['){
+                s.push(ch);
+             }
+             else{
+                
+                if(s.isEmpty()){
+                    return false;
+                }
+                //closing
+                if(s.peek() =='{' && ch =='}' || s.peek() =='(' && ch == ')' || s.peek() =='[' &&  ch == ']'){
+                    s.pop();
+                }
+                else{
+                    return false;
+                }
+             }
+        }
+             // s is empty...?
+             if(s.isEmpty()){
+                return true;
+             }
+        
+        return false;
+    }
+}
